29e2b28dfdb2b851f6c64e0e873ff42f1fa82136
Juan Silva Suarez <juanseba.96@hotmail.com>
2024-08-29 18:58:57 -0400

    cambios ortograficos

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from contactos.eliminar import *
 
 def menu():
     input("Presione enter \n")
-    print("Bienvenido al menu \n")
+    print("Bienvenido al menú \n")
+    print("Ingrese un número para acceder a la funcionalidad \n")
     print("[1]. Agregar contacto \n")
     print("[2]. Buscar contacto \n")
     print("[3]. Mostrar Todo \n")
     print("[4]. Actualizar \n")
     print("[5]. Eliminar \n")
     print("[6-9]. Salir \n")
-    opc = int(input("Seleccione una opcion \n"))
+    opc = int(input("Seleccione una opción \n"))
 
     if opc == 1:
-        print("Bienvenido al menu de ingreso \n")
+        print("Bienvenido al menú de ingreso \n")
         nombre = input("Ingrese el nombre \n")
         correo = input("Ingrese el correo \n")
         agregar_contacto(nombre, correo)
@@ -24,17 +25,17 @@ def menu():
         mostrar_contacto()
         menu()
     elif opc == 2:
-        print("Bienvenido al menu de busqueda \n")
+        print("Bienvenido al menú de búsqueda \n")
         nombre = input("Ingrese el nombre \n")
         buscar_contacto(nombre)
         menu()
     elif opc == 3:
-        print("Bienvenido al menu de mostrar \n")
+        print("Bienvenido al menú de mostrar \n")
         print("Los contactos registrados son: \n")
         mostrar_contacto()
         menu()
     elif opc == 4:
-        print("Bienvenido al menu de actualizar \n")
+        print("Bienvenido al menú de actualizar \n")
         mostrar_contacto()
         indice = int(input("Seleccione cual desea modificar \n"))
         nombre = input("Ingrese el nuevo nombre \n")
@@ -42,7 +43,7 @@ def menu():
         actualizar_contacto(indice, nombre, correo)
         menu()
     elif opc == 5:
-        print("Bienvenido al menu de eliminar \n")
+        print("Bienvenido al menú de eliminar \n")
         mostrar_contacto()
         indice = int(input("Seleccione cual desea modificar \n"))
         eliminar_contacto(indice)
